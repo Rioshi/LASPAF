f8328aae51df8595093074f1c7a90b32a0b1628c
Carlos Julian Mestanza Novoa <julian.cmn@gmail.com>
2019-08-04 04:07:14 -0500

    Update SAR_modelling.R

--- SAR_modelling.R
+++ SAR_modelling.R
@@ -17,7 +17,7 @@ laspaf <- gap %>%
 laspaf <- as.data.frame(laspaf)
 rm(my_sheets,gap)
 
-laspaf$SAR <- (laspaf$`Na+`)/sqrt((laspaf$`Ca2+`+laspaf$`Mg2+`)/2)
+laspaf$SAR <- (laspaf$Nas)/sqrt((laspaf$Cas+laspaf$Mgs)/2)
 laspaf$PSI <- (laspaf$Na)*100/laspaf$CIC
 
 ##################################
@@ -27,24 +27,56 @@ str(laspaf)
 
 #dt$Age[dt$Age == 99] <- NA para indicar valores NA
 
-mod1 <- lm(laspaf$SAR~laspaf$PSI,na.action = na.omit)
+mod1 <- lm(SAR~PSI,na.action = na.omit,data=laspaf)
 summary(mod1)
-
+plot(mod1)
 plot(x=laspaf$PSI,y=laspaf$SAR)
 
+car::ncvTest(mod1)
+laspaf1 <- subset(laspaf,SAR<40)
+mod2 <- lm(SAR~PSI,na.action = na.omit,data=laspaf1)
+plot(mod2)
+car::ncvTest(mod2)
 
-laspaf1 <- subset(laspaf,PSI > 80)
-plot(x=lala$PSI,y=lala$SAR)
-
-lala<-subset(laspaf,SAR < 40)
-hist(lala$SAR)
-mod2 <- lm(lala$SAR~lala$PSI,na.action = na.omit)
-summary(mod2)
 atipico <- lala[c(3917,3927,4899),]
 atipico <- laspaf[c(93),]
 
-mod3 <- glm(lala$SAR~lala$PSI,family=Gamma(link=identity))
+##################################
+#Modelamiento GLM Gamma
+##################################
+mod3 <- glm(SAR~PSI,family=Gamma(link=identity),data=laspaf1)
 summary(mod3)
 plot(mod3)
-source('http://www.poleto.com/funcoes/envel.gama.txt')
-envel.gama(mod3)
+boot:: glm.diag.plots(mod3)
+
+car::residualPlots(mod3)
+car::influenceIndexPlot(mod3)
+
+# Utilizar la distancia de cook
+numero <- which(cooks.distance(mod3) > 0.01)
+plot(cooks.distance(mod3),type="h")
+
+atipico <- laspaf1[c(numero),]
+
+
+mod4 <- glm(SAR~PSI,family=Gamma(link=log),data=laspaf1)
+summary(mod4)
+boot:: glm.diag.plots(mod4)
+car::residualPlots(mod4)
+car::influenceIndexPlot(mod4)
+
+#source('http://www.poleto.com/funcoes/envel.gama.txt')
+#source('http://www.poleto.com/funcoes/diag.gama.txt')
+#diag.gama(mod3)
+#envel.gama(mod3)
+
+##################################
+#Modelamiento GLM doble
+##################################
+require(dglm)
+mod5 = dglm(SAR~PSI, ~ PSI,
+            family=Gamma(link=identity),dlink = "log",
+            data=laspaf1)
+summary(mod5)
+#AIC = -2*loglikelihood +2k || k: numero de parametos
+2*(2+2) - mod5$m2loglik
